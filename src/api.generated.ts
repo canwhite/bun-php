436de61db3f6872069e59e9679206295dcb9c7dc
qiaochao <752716886@qq.com>
2026-01-24 16:20:40 +0800

    feat: add ignore itemss

--- src/api.generated.ts
+++ src/api.generated.ts
@@ -1,7 +1,7 @@
 // ==============================================
 // AUTO-GENERATED - DO NOT EDIT MANUALLY!
 // API路由配置
-// Generated at: 2026-01-24T08:11:14.885Z
+// Generated at: 2026-01-24T08:19:21.748Z
 // ==============================================
 
 import type { ApiRouteConfig } from './lib/router/types';
@@ -125,7 +125,7 @@ export const routesByFilePath: Record<string, ApiRouteConfig> = {
 
 export const config = {
   rootDir: "src/app/api",
-  generatedAt: "2026-01-24T08:11:14.885Z",
+  generatedAt: "2026-01-24T08:19:21.748Z",
   version: "1.0.0",
 };
 
--- src/routes.generated.ts
+++ src/routes.generated.ts
@@ -1,7 +1,7 @@
 // ==============================================
 // AUTO-GENERATED - DO NOT EDIT MANUALLY!
 // 文件路由配置
-// Generated at: 2026-01-24T08:11:14.718Z
+// Generated at: 2026-01-24T08:19:21.435Z
 // ==============================================
 
 
@@ -303,7 +303,7 @@ export const routesByFilePath: Record<string, RouteConfig> = {
 
 export const config = {
   rootDir: "src/app/",
-  generatedAt: "2026-01-24T08:11:14.718Z",
+  generatedAt: "2026-01-24T08:19:21.435Z",
   version: "1.0.0",
 };
 
